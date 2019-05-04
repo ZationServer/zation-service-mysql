0a73b0229a55f39edf07d2d6e120c5c49864741e
Luca Scaringella <lucalstyle@gmail.com>
2019-05-04 17:45:10 +0200

    Update jsDoc, improve code

--- src/lib/mySqlModule.ts
+++ src/lib/mySqlModule.ts
@@ -4,16 +4,16 @@ GitHub: LucaCode
 ©Copyright by Luca Scaringella
  */
 
-import MySql = require("mysql");
+import MySql         = require("mysql");
 import {ServiceModule} from "zation-service";
-import {PoolConfig} from "mysql";
-import {SmallBag,Bag} from "zation-server";
+import {PoolConfig}    from "mysql";
+import {SmallBag}      from "zation-server";
 
 const serviceName = "MySql";
 
 export namespace MySqlModule {
 
-    export function build(configs: Record<string, PoolConfig> | DefaultConfig<PoolConfig>): ServiceModule<PoolConfig, MySql.Pool, {}> {
+    export function build(configs: Record<string, PoolConfig> | DefaultConfig<PoolConfig>): ServiceModule<PoolConfig, MySql.Pool,{},SmallBagExtension> {
         const service: any = configs;
         service.get = undefined;
         service.create = async (c: PoolConfig): Promise<MySql.Pool> => {
@@ -37,10 +37,10 @@ export namespace MySqlModule {
             service: service,
             bagExtensions: {
                 smallBag : {
-                    mySqlQuery : async function (this : SmallBag,query ,serviceKey : string = 'default') : Promise<{results : any,fields : MySql.FieldInfo[]}> {
+                    mySqlQuery : async function (this : SmallBag,query ,configName : string = 'default') : Promise<{results : any,fields : MySql.FieldInfo[]}> {
                         return new Promise<{results : any,fields : MySql.FieldInfo[]}>(async (resolve, reject) =>
                         {
-                            (await this.getService<MySql.Pool>(serviceName,serviceKey)).
+                            (await this.getService<MySql.Pool>(serviceName,configName)).
                             query(query,(error, results : any, fields : MySql.FieldInfo[]) => {
                                 if(error) {reject(error);}
                                 else{resolve({results : results, fields : fields});}
@@ -50,16 +50,15 @@ export namespace MySqlModule {
                     mySqlFormat : function(query: string, inserts: any[], stringifyObjects?: boolean, timeZone?: string) : string {
                         return MySql.format(query,inserts,stringifyObjects,timeZone);
                     },
-                    getMySql : async function(this : SmallBag,serviceKey : string = 'default') : Promise<MySql.Pool>
+                    getMySql : async function(this : SmallBag,configName : string = 'default') : Promise<MySql.Pool>
                     {
-                        return await this.getService<MySql.Pool>(serviceName,serviceKey);
+                        return await this.getService<MySql.Pool>(serviceName,configName);
                     },
-                    isMySql : function (this : SmallBag,serviceKey : string = 'default') : boolean
+                    isMySql : function (this : SmallBag,configName : string = 'default') : boolean
                     {
-                        return this.isService(serviceKey,serviceKey);
+                        return this.isService(serviceName,configName);
                     }
-                },
-                bag : {}
+                }
             }
         };
     }
@@ -70,57 +69,57 @@ interface DefaultConfig<T> {
     default?: T;
 }
 
-declare module 'zation-server' {
-    export interface SmallBag {
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Is for an mysql query.
-         * Throws an ServiceNotFoundError if the service is not found.
-         * @example
-         * const res = mySqlQuery('SELECT 1 + 1 AS solution');
-         * const solution = res.results[0];
-         * @throws ServiceNotFoundError
-         * @param  query
-         * @param  serviceKey
-         * the key to the service.
-         * @return Promise<object>
-         * The object has 2 fields, one for the result 'result' and one for the fields information 'fields'.
-         */
-        mySqlQuery: (query, serviceKey ?: string) => Promise<{ results: any, fields: MySql.FieldInfo[] }>
+interface SmallBagExtension
+{
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * @description
+     * Make a simple MySQL query.
+     * Throws an ServiceNotFoundError if the service is not found.
+     * @example
+     * const res = await mySqlQuery('SELECT 1 + 1 AS solution');
+     * const solution = res.results[0];
+     * @throws ServiceNotFoundError
+     * @param  query
+     * @param  configName
+     * @return Promise<object>
+     * The object has 2 fields, one for the result 'result' and one for the fields information 'fields'.
+     */
+    mySqlQuery: (query, configName ?: string) => Promise<{ results: any, fields: MySql.FieldInfo[] }>
 
-        // noinspection JSMethodCanBeStatic,JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Format an mySql query.
-         * @example
-         * mySqlFormat('SELECT * FROM ?? WHERE ?? = ?',['users', 'id', 10]);
-         * @param  query
-         * @param inserts
-         * @param stringifyObjects?
-         * @param  timeZone?
-         * @return string
-         */
-        mySqlFormat: (query: string, inserts: any[], stringifyObjects?: boolean, timeZone?: string) => string
+    // noinspection JSMethodCanBeStatic,JSUnusedGlobalSymbols
+    /**
+     * @description
+     * Format a MySQL query.
+     * @example
+     * mySqlFormat('SELECT * FROM ?? WHERE ?? = ?',['users', 'id', 10]);
+     * @param  query
+     * @param inserts
+     * @param stringifyObjects?
+     * @param  timeZone?
+     * @return string
+     */
+    mySqlFormat: (query: string, inserts: any[], stringifyObjects?: boolean, timeZone?: string) => string
 
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Returns this service, if it exist otherwise it will throw an ServiceNotFoundError error.
-         * @throws ServiceNotFoundError
-         * @param  serviceKey
-         * the key to the service.
-         */
-        getMySql(serviceKey ?: string): Promise<MySql.Pool>
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * @description
+     * Returns this MySQL service, if it exists otherwise, it will throw a ServiceNotFoundError error.
+     * @throws ServiceNotFoundError
+     * @param  configName
+     */
+    getMySql(configName ?: string): Promise<MySql.Pool>
 
-        // noinspection JSUnusedGlobalSymbols
-        /**
-         * @description
-         * Checks if the service with this key is exist and can be used.
-         * @param  serviceKey
-         * the key to the service.
-         */
-        isMySql(serviceKey ?: string): boolean
-    }
+    // noinspection JSUnusedGlobalSymbols
+    /**
+     * @description
+     * Checks if the service with this configuration exists.
+     * @param  configName
+     */
+    isMySql(configName ?: string): boolean
 }
 
+declare module 'zation-server' {
+    export interface SmallBag extends SmallBagExtension {
+    }
+}
